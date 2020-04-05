8cd2f2a594dd0453b15d9446b50270261a484918
Anton <antonburchak01@gmail.com>
2020-04-05 18:39:18 +0300

    _compact function created

--- functions/array.js
+++ functions/array.js
@@ -1,10 +1,21 @@
-// @ Developed by Anton Burchak
-// See my LinkedIn profile  :: ///
-// View my github portfolio ::
-// 1/324
 
+/*
+    @ Developed by Anton Burchak
+    Own Lodash implementation
+    Lodash version: https://lodash.com/docs/4.17.15
+    See my LinkedIn profile  :: https://www.linkedin.com/in/antonburchak/
+    View my github portfolio :: https://github.com/AntonBurchak
 
-const chunk = (array = [], size = 1) => { // Success
+    Progress:
+    1/324 Solved
+
+    last update: 05.04.2020
+
+    READ: This .js file has inside only array functions
+*/
+
+// [X] Solved
+const chunk = (array = [], size = 1) => { 
     if (array instanceof Array && !isNaN(+size)) {
         const result_array = [];
         let   temp_array = [];
@@ -25,7 +36,15 @@ const chunk = (array = [], size = 1) => { // Success
         throw new Error('Invalid arguments types');
     }
 }
-
+// Input:  ['a', 'b', 'c', 'd', 22, 224], 2
+// Output: [ [ 'a', 'b' ], [ 'c', 'd' ], [ 22, 224 ] ]
 // console.log(chunk(['a', 'b', 'c', 'd', 22, 224], 2))
 
 
+const compact = (array) => {
+    const config = [false, null, 0, "", undefined, NaN] // falsey types
+    return array.filter(element => config.indexOf(element) == -1 ? true : false);
+}
+// Input:  [0, 1, false, 2, '', 3]
+// Output: [ 1, 2, 3 ]
+// console.log(compact([0, 1, false, 2, '', 3]))
